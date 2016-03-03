b31a1f2f8d271adebe71b4ebccf5752134a87fed
Andreas Østensen <androste@ntnu.no>
2016-03-03 15:19:33 +0100

    First commit

---
+++ src/FDC1004_differential.cpp
@@ -0,0 +1,139 @@
+/***********************************************************************
+FDC1004_differential Library
+
+This library provides functions for using TI's FDC1004 Capacitance to Digital Sensor in differential measurement
+
+Written by Andreas Østensen
+NTNU, Norway
+
+************************************************************************/
+
+#include "FDC1004_differential.h"
+
+uint8_t MEAS_CONFIG[] = { 0x08, 0x09, 0x0A, 0x0B };
+uint8_t MEAS_MSB[] = { 0x00, 0x02, 0x04, 0x06 };
+uint8_t MEAS_LSB[] = { 0x01, 0x03, 0x05, 0x07 };
+uint8_t SAMPLE_DELAY[] = { 11, 11, 6, 3 };
+
+FDC1004::FDC1004(uint16_t rate) {
+	this->_rate = rate;
+}
+
+//Read function
+uint16_t FDC1004::read16(uint8_t reg) {
+	Wire.beginTransmission(FDC1004_ADDRESS);
+	Wire.write(reg);
+	Wire.endTransmission();
+	uint16_t value;
+	Wire.beginTransmission(FDC1004_ADDRESS);
+	Wire.requestFrom(FDC1004_ADDRESS, (uint8_t)2);
+	value = Wire.read();
+	value <<= 8;
+	value |= Wire.read();
+	Wire.endTransmission();
+	return value;
+}
+
+//Write function
+void FDC1004::write16(uint8_t reg, uint16_t data) {
+	Wire.beginTransmission(FDC1004_ADDRESS);
+	Wire.write(reg); //send address
+	Wire.write((uint8_t)(data >> 8));
+	Wire.write((uint8_t)data);
+	Wire.endTransmission();
+}
+
+uint8_t FDC1004::configureMeasurement(uint8_t measurement, uint8_t channel_1, uint8_t channel_2) {
+	//Verify data
+	if (!FDC1004_IS_MEAS(measurement) || !FDC1004_IS_CHANNEL(channel_1) || !FDC1004_IS_CHANNEL(channel_2) || (channel_1 == channel_2)) {
+		Serial.println("bad measurement configuration");
+		return 1;
+	}
+
+	//build 16 bit configuration
+	uint16_t configuration_data;
+	configuration_data = ((uint16_t)channel_1) << 13; //CHA
+	configuration_data |= ((uint16_t)channel_2) << 10; //CHB
+	write16(MEAS_CONFIG[measurement], configuration_data);
+	return 0;
+}
+
+uint8_t FDC1004::triggerSingleMeasurement(uint8_t measurement, uint8_t rate) {
+	//verify data
+	if (!FDC1004_IS_MEAS(measurement) || !FDC1004_IS_RATE(rate)) {
+		Serial.println("bad trigger request");
+		return 1;
+	}
+	uint16_t trigger_data;
+	trigger_data = ((uint16_t)rate) << 10; // sample rate
+	trigger_data |= 0 << 8; //repeat disabled
+	trigger_data |= (1 << (7 - measurement)); // 0 > bit 7, 1 > bit 6, etc
+	write16(FDC_REGISTER, trigger_data);
+	return 0;
+}
+
+uint8_t FDC1004::readMeasurement(uint8_t measurement, uint16_t value[]) {
+	if (!FDC1004_IS_MEAS(measurement)) {
+		Serial.println("bad read request");
+		return 1;
+	}
+
+	//check if measurement is complete
+	uint16_t fdc_register = read16(FDC_REGISTER);
+	if (!(fdc_register & (1 << (3 - measurement)))) {
+		Serial.println("measurement not completed");
+		return 2;
+	}
+
+	//read the value
+	uint16_t msb = read16(MEAS_MSB[measurement]);
+	uint16_t lsb = read16(MEAS_LSB[measurement]);
+	value[0] = msb;
+	value[1] = lsb;
+	return 0;
+}
+
+uint8_t FDC1004::measureChannel(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate, uint16_t * value) {
+	if (configureMeasurement(measurement, channel_1, channel_2)) return 1;
+	if (triggerSingleMeasurement(measurement, rate)) return 1;
+	delay(SAMPLE_DELAY[rate]);
+	return readMeasurement(measurement, value);
+}
+
+double FDC1004::getCapacitance(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate) {
+	int32_t value;
+	uint8_t result = getRawCapacitance(measurement, channel_1, channel_2, rate, &value);
+	if (result) return 0x80000000;
+
+	return (double)value / PICOFARAD_CONVERSION_CONSTANT; //picofarads
+}
+
+uint8_t FDC1004::getRawCapacitance(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate, int32_t * value) {
+	if (!FDC1004_IS_CHANNEL(channel_1) || !FDC1004_IS_CHANNEL(channel_1) || (channel_1 == channel_2)) return 1;
+	uint16_t raw_value[2];
+
+	if (measureChannel(measurement, channel_1, channel_2, rate, raw_value)) {
+		Serial.println("error");
+		return 1;
+	}
+
+	*value = ((uint32_t)raw_value[1] >> 8) | ((uint32_t)raw_value[0] << 8);
+
+	return 0;
+}
+
+double FDC1004::measureLevel(double base_cap, double prefactor, uint8_t rate) {
+	/*
+	MEAS1 = CIN1(CHA) - CIN4(CHB) //Level sensor
+	MEAS2 = CIN2(CHA) - CIN4(CHB) //Reference Environment sensor
+	MEAS3 = CIN3(CHA) - CIN4(CHB) //Reference Level sensor
+
+	level = h_RL * (C_level - C_level(0)) / (C_RL - C_RE)
+	*/
+
+	double c_level = getCapacitance(FDC1004_MEAS1, FDC1004_CIN1, FDC1004_CIN4, rate);
+	double c_rl = getCapacitance(FDC1004_MEAS2, FDC1004_CIN2, FDC1004_CIN4, rate);
+	double c_re = getCapacitance(FDC1004_MEAS3, FDC1004_CIN3, FDC1004_CIN4, rate);
+
+	return prefactor * ((c_level - base_cap) / (c_rl - c_re) * -1);
+}
---
+++ src/FDC1004_differential.h
@@ -0,0 +1,68 @@
+// FDC1004_differential.h
+/************************
+This is a library for differential measurement with the FDC1004 Capacitive Sensor.
+Created for use with the Capacitive-Based Liquid Level Sensing Sensor Reference Design:
+http://www.ti.com/lit/pdf/tidu736
+
+Written by Andreas Østensen
+Student at NTNU, Norway
+************************/
+
+#ifndef _FDC1004_DIFFERENTIAL_h
+#define _FDC1004_DIFFERENTIAL_h
+
+	#include "Arduino.h"
+	#include "Wire.h"
+
+	//Constants and limits for FDC1004
+	#define FDC1004_ADDRESS (uint8_t)0b1010000
+
+	#define FDC1004_100HZ (0x01)
+	#define FDC1004_200HZ (0x02)
+	#define FDC1004_400HZ (0x03)
+	#define FDC1004_IS_RATE(x) (x == FDC1004_100HZ || x == FDC1004_200HZ || x == FDC1004_400HZ)
+
+	#define FDC1004_CHANNEL_MAX (0x03)
+	#define FDC1004_IS_CHANNEL(x) (x >= 0 && x <= FDC1004_CHANNEL_MAX)
+	#define FDC1004_CIN1 0
+	#define FDC1004_CIN2 1
+	#define FDC1004_CIN3 2
+	#define FDC1004_CIN4 3
+
+
+	#define FDC1004_MEAS_MAX (0x03)
+	#define FDC1004_IS_MEAS(x) (x >= 0 && x <= FDC1004_MEAS_MAX)
+	#define FDC1004_MEAS1 0
+	#define FDC1004_MEAS2 1
+	#define FDC1004_MEAS3 2
+	#define FDC1004_MEAS4 3
+
+	#define PICOFARAD_CONVERSION_CONSTANT (524288) // 2^19
+
+	#define FDC_REGISTER (0x0C)
+
+	/******************************************************************************************
+	* Function Declarations
+	*******************************************************************************************/
+	class FDC1004 {
+	public:
+		FDC1004(uint16_t rate = FDC1004_100HZ);
+		uint16_t read16(uint8_t reg);
+		void write16(uint8_t reg, uint16_t data);
+		uint8_t configureMeasurement(uint8_t measurement, uint8_t channel_1, uint8_t channel_2);
+		uint8_t triggerSingleMeasurement(uint8_t measurement, uint8_t rate);
+		uint8_t readMeasurement(uint8_t measurement, uint16_t value[]);
+		uint8_t measureChannel(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate, uint16_t * value);
+		double getCapacitance(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate);
+		uint8_t getRawCapacitance(uint8_t measurement, uint8_t channel_1, uint8_t channel_2, uint8_t rate, int32_t * value);
+		double measureLevel(double base_cap, double prefactor, uint8_t rate);
+
+	private:
+		double value;
+		double _base_cap;
+		uint8_t _rate;
+	};
+
+
+#endif
+
